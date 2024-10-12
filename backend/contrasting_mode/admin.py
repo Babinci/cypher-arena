6e58e70a131203bebaee3e8e68fa409aed5d61f7
Babinci <walkowo@gmail.com>
2024-10-12 19:02:58 +0200

    admin contrast

--- backend/contrasting_mode/admin.py
+++ backend/contrasting_mode/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ContrastPair
+
+class ContrastPairAdmin(admin.ModelAdmin):
+    list_display = ('item1', 'item2', 'rating')
+
+admin.site.register(ContrastPair, ContrastPairAdmin)
+
 
 # Register your models here.
