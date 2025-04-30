f70b44475808c30904b53e75ea3d67d9b6dfa36a
Wojtek <walkowo@gmail.com>
2025-04-30 20:07:25 +0200

    automatic commit

--- backend/words/models.py
+++ backend/words/models.py
@@ -6,7 +6,7 @@ class Word(models.Model):
     name = models.CharField(max_length=255, unique=True)
     occurrence = models.PositiveIntegerField()
     speech_part = models.CharField(null=True, max_length=1024)
-
+    vector_embedding = models.BinaryField(null=True, blank=True)
     def __str__(self):
         return self.name
 
@@ -14,7 +14,7 @@ class Word(models.Model):
 class Temator(models.Model):
     name = models.CharField(max_length=511, unique=True)
     source = models.CharField(max_length=100, default='standard')
-
+    vector_embedding = models.BinaryField(null=True, blank=True)
 
 class ContrastPair(models.Model):
     item1 = models.CharField(max_length=100)
