3991604a7b861b96ef89560b9e11ef7180eeb9de
Wojtek <walkowo@gmail.com>
2025-05-01 18:05:34 +0200

    automatic commit

--- backend/words/admin.py
+++ backend/words/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
-from .models import Word, ContrastPair, CypherArenaPerplexityDeepResearch
+from .models import Word, ContrastPair, CypherArenaPerplexityDeepResearch, ContrastPairRating
 
 # Register your models here.
 class WordsAdmin(admin.ModelAdmin):
     list_display = ('name', 'occurrence', 'speech_part')
 
+class ContrastPairRatingFilter(admin.SimpleListFilter):
+    title = 'Rating'
+    parameter_name = 'rating'
+
+    def lookups(self, request, model_admin):
+        # Show all possible rating values (1-5)
+        return [(str(i), str(i)) for i in range(1, 6)]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            # Filter ContrastPairs that have at least one rating with the selected value
+            return queryset.filter(ratings__rating=self.value()).distinct()
+        return queryset
+
 class ContrastPairAdmin(admin.ModelAdmin):
     list_display = ('item1', 'item2', )
+    list_filter = (ContrastPairRatingFilter,)
 
 
 class CypherArenaPerplexityDeepResearchAdmin(admin.ModelAdmin):
