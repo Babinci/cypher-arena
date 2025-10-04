e9974e4d4e172f1e193b9a195345923cd5e93fcd
Wojtek <walkowo@gmail.com>
2025-10-04 17:42:41 +0200

    automatic

---
+++ ai_agent/api.py
@@ -0,0 +1,10 @@
+example_payload = {
+  "news_sources": [
+    {
+      "content": "string",
+      "news_date": "2025-10-04",
+      "news_source": "string",
+      "ai_agent": "string"
+    }
+  ]
+}
