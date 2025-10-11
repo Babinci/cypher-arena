777cd1ff40c85621f0593abdc8eb3384f8b797dc
Wojtek <walkowo@gmail.com>
2025-10-11 10:49:43 +0200

    automatic

--- ai_agent/langchain_scripts/test_my_llms_api.py
+++ ai_agent/langchain_scripts/test_my_llms_api.py
@@ -24,22 +24,12 @@ OPEN_ROUTER_API_KEY = os.getenv("OPEN_ROUTER_API_KEY")
 
 llm = ChatGoogleGenerativeAI(model="models/gemini-flash-lite-latest", google_api_key=GOOGLE_API_KEY)
 
-print('Invoking Gemini LLM...')
-result_gemini = time_llm_invoke(llm.invoke)("What is the capital of France? Answer in one word.")
-print(result_gemini.content)
-
-
-
 llm_glm = ChatOpenAI(
     model="glm-4.6",
     temperature=0,
     openai_api_key=GLM_API_KEY,        # any non-empty string
     openai_api_base="https://api.z.ai/api/coding/paas/v4"  # from WSL, try this first
 )
-
-print('Invoking GLM 4.6  LLM...')
-result_glm = time_llm_invoke(llm_glm.invoke)("What is the capital of Italy? Answer in one word.")
-print(result_glm.content)
 
 llm_glm_air = ChatOpenAI(
     model="glm-4.5-air",
@@ -48,18 +38,26 @@ llm_glm_air = ChatOpenAI(
     openai_api_base="https://api.z.ai/api/coding/paas/v4"  # from WSL, try this first
 )
 
-
-print('Invoking GLM air LLM...')
-result_glm_air = time_llm_invoke(llm_glm_air.invoke)("What is the capital of Poland? Answer in one word.")
-print(result_glm_air.content)
-
 llm_openrouter=ChatOpenAI(
     model="meta-llama/llama-4-maverick:free",
     openai_api_key=OPEN_ROUTER_API_KEY,
     openai_api_base="https://openrouter.ai/api/v1"
 )
 
-print('Invoking OpenRouter LLM...')
-result_openrouter = time_llm_invoke(llm_openrouter.invoke)("What is the capital of Germany? Answer in one word.")
-print(result_openrouter.content)
+if __name__ == "__main__":
+    print('Invoking Gemini LLM...')
+    result_gemini = time_llm_invoke(llm.invoke)("What is the capital of France? Answer in one word.")
+    print(result_gemini.content)
+
+    print('Invoking GLM 4.6  LLM...')
+    result_glm = time_llm_invoke(llm_glm.invoke)("What is the capital of Italy? Answer in one word.")
+    print(result_glm.content)
+
+    print('Invoking GLM air LLM...')
+    result_glm_air = time_llm_invoke(llm_glm_air.invoke)("What is the capital of Poland? Answer in one word.")
+    print(result_glm_air.content)
+
+    print('Invoking OpenRouter LLM...')
+    result_openrouter = time_llm_invoke(llm_openrouter.invoke)("What is the capital of Germany? Answer in one word.")
+    print(result_openrouter.content)
 
